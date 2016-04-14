9de9ac7df5d25194b6e86a3a048b8cf68883fd03
jhillon <jhillon@heinsohn.com.co>
2016-04-13 20:48:09 -0500

    Actualización de avance hasta creación de servicios

---
+++ AuthSln/AuthWeb/app/app.js
@@ -0,0 +1,21 @@
+﻿angular.module('AngularAuthApp', [
+    // Angular modules     
+    'ngRoute',
+    // Custom modules 
+
+    // 3rd Party Modules
+    'ui.router',
+    'angular-loading-bar',
+    'LocalStorageModule'
+]);
+
+
+angular.module('AngularAuthApp')
+    .run(['authService', function (authService) {
+        authService.fillAuthData();
+    }]);
+
+angular.module('AngularAuthApp')
+    .config(function ($httpProvider) {
+        $httpProvider.interceptors.push('authInterceptorService');
+    });
---
+++ AuthSln/AuthWeb/app/states/states.js
@@ -0,0 +1,69 @@
+﻿/*app.config(function ($routeProvider) {
+
+    $routeProvider.when("/home", {
+        controller: "homeController",
+        templateUrl: "/app/views/home.html"
+    });
+
+    $routeProvider.when("/login", {
+        controller: "loginController",
+        templateUrl: "/app/views/login.html"
+    });
+
+    $routeProvider.when("/signup", {
+        controller: "signupController",
+        templateUrl: "/app/views/signup.html"
+    });
+
+    $routeProvider.when("/orders", {
+        controller: "ordersController",
+        templateUrl: "/app/views/orders.html"
+    });
+
+    $routeProvider.otherwise({ redirectTo: "/home" });
+});*/
+
+angular.module('AngularAuthApp')
+    .config(function ($stateProvider, $urlRouterProvider) {
+        $urlRouterProvider.otherwise('/home');
+        $stateProvider
+            .state('home', {
+                url: '/home',
+                data: {
+                    authorities: ['ROLE_USER'],
+                    pageTitle: 'ekoguiApp.persona.home.title'
+                },
+                views: {
+                    'content@': {
+                        templateUrl: '/app/views/home.html',
+                        controller: 'homeController'
+                    }
+                }
+            })
+            .state('login', {
+                url: '/login',
+                data: {
+                    authorities: ['ROLE_USER'],
+                    pageTitle: 'ekoguiApp.persona.home.title'
+                },
+                views: {
+                    'content@': {
+                        templateUrl: '/app/views/login.html',
+                        controller: 'loginController'
+                    }
+                }
+            })
+            .state('signup', {
+                url: '/signup',
+                data: {
+                    authorities: ['ROLE_USER'],
+                    pageTitle: 'ekoguiApp.persona.home.title'
+                },
+                views: {
+                    'content@': {
+                        templateUrl: '/app/views/signup.html',
+                        controller: 'signupController'
+                    }
+                }
+            })
+    });
